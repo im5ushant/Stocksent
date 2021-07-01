6a54aeca5ba4a93fa7abe73e229e0bf7627016a3
im5ushant <40163301+im5ushant@users.noreply.github.com>
2021-07-02 05:09:15 +0530

    Delete twitter_credentials.py

--- backend/twitter_credentials.py
+++
@@ -1,5 +0,0 @@
-# Variables that contain the user credentials to access Twitter API
-ACCESS_TOKEN = "1712278842-X9BnFQrDxCWPXgnE47aOgbrawfDCDPJpVhbDWhn"
-ACCESS_TOKEN_SECRET = "fmgh0CgZqchFLDUjy4x9t2mjMA0Z1pALzsTxKwIzqZHjG"
-CONSUMER_KEY = "bEOGZWmJ9ueaoBgm2hzhEsZu6"
-CONSUMER_SECRET = "TRjFBevGsygUGG0Tk7Nnvtm08W2E5N2yIDEbEZWlilzrccgigD"
